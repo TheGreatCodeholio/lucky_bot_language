ea3da9ee73653dabae00eed609586c5550efc878
TheGreatCodeholio <ian@icarey.net>
2022-05-26 11:54:29 -0400

    Initial commit

---
+++ de.py
@@ -0,0 +1,32 @@
+de = {
+    "group_lang": {
+        "inv": "Ungültige Sprachoption.",
+        "message_1": "Deutsch: An",
+        "message_2": "Deutsch ist bereits aktiviert.",
+        "message_3": "Nur Admins können die Sprache ändern."
+    },
+    "group_users": {
+        "denied_1": "Nur Admins können {} abfragen.",
+    },
+    "group_fun": {
+        "greeting_1": "Hallo.",
+
+    },
+    "group_status": {
+        "group_name_change": "Group name changed to:",
+        "promote": "Promoted",
+        "demote": "Demoted"
+    },
+    "feature_status": {
+        "set_1a": "Aus",
+        "set_1b": "An",
+        "set_3a": "kann nur ein Admin deaktivieren",
+        "set_3b": "kann nur ein Admin aktivieren",
+        "set_4a": "ist bereits aus",
+        "set_4b": "ist bereits an",
+        "set_5": "is not set.",
+        "set_msg_1": "Nachricht kann nur ein Admin einstellen",
+        "set_msg_2a": "eingestellt auf",
+
+    }
+}
---
+++ es.py
@@ -0,0 +1,30 @@
+es = {
+    "group_lang": {
+        "inv": "Opción de Idioma no Válida.",
+        "message_1": "Español: Activado",
+        "message_2": "Español ya activado.",
+        "message_3": "El idioma solo puede ser cambiado por administradores."
+    },
+    "group_users": {
+        "denied_1": "Sólo Administradores Pueden Solicitar {}.",
+    },
+    "group_fun": {
+        "greeting": "Hola",
+    },
+    "group_status": {
+        "group_name_change": "Group name changed to:",
+        "promote": "Promoted",
+        "demote": "Demoted"
+    },
+    "feature_status": {
+        "set_1a": "Desactivado",
+        "set_1b": "Activado",
+        "set_3a": "sólo puede ser desactivado por un administrador",
+        "set_3b": "sólo puede ser activado por un administrador",
+        "set_4a": "ya está desactivado",
+        "set_4b": "ya está activado",
+        "set_5": "is not set.",
+        "set_msg_1": "el mensaje sólo puede ser establecido por un administrador",
+        "set_msg_2a": "configurado a"
+    }
+}
